27fc45e35bc2df3d36c20f82b15f3a6e9d2e15a8
Alexander Manenko <manenko@gmail.com>
2013-03-09 11:07:58 +0200

    Fix \c and \p annotations to correctly highlight methods references like IUnknown::AddRef.

--- Enhanced/Properties/AssemblyInfo.cs
+++ Enhanced/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Enhanced")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
 [assembly: AssemblyProduct("EnhancedCommentsCpp")]
 [assembly: AssemblyCopyright("Copyright (c) 2013 by Alexander Manenko")]
 [assembly: ComVisible(false)]
